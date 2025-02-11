6b94c518aca30feb84f82f881c5a7c427002f6f5
jab0n <165092720+jab0n@users.noreply.github.com>
2025-02-10 18:14:07 -0600

    FizzBuzz.hs

--- FizzBuzz.hs
+++ FizzBuzz.hs
@@ -1,21 +1,21 @@
 module FizzBuzz where
 
-lessThan20 :: Int -> String
+lessThan20 :: Int -> String -- Regresa el nombre de la unidad y los numeros del 1 al 19
 lessThan20 n
     | n > 0 && n <20 =  --El número es mayor a 0 y menor a 20
         let answers = words "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen"  --Nombre de los numeros del 1 al 19
         in answers !! (n-1)
 
-tens :: Int -> String
+tens :: Int -> String -- Regresa el nombre de la decena
 tens n
     | n > 1 && n < 10 =    --El número es mayor a 1 y menor a 10
         answers !! (n-2)
         where
             answers = words "twenty thirty forty fifty sixty seventy eighty ninety"  -- Nombre de las decenas
 
-number :: Int -> String 
+number :: Int -> String -- Funcuón para FizzBuzz
 number n
-    | n < 0                                         = "Fuera del rango"     -- El número es menor a 0, por lo que no entra dentro de rango 
+    | n < 0 || n > 100                              = "Fuera del rango"     -- El número es menor a 0, por lo que no entra dentro de rango 
     | n == 0                                        = "zero"    --En número es igual a 0
     | n `mod` 5 == 0 && n `mod` 3 == 0 && n <= 100  = "FizzBuzz"    --El número es multiplo de 5 y de 3, ademas de ser menor o igual a 100
     | n `mod` 5 == 0 && n <= 100                    = "Fizz"    --El número es multiplo de 5, ademas de ser menor o igual a 100
