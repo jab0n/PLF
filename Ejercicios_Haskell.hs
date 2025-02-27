5f6db098ecebe1539ead52cd0faff87033dc9a8d
jab0n <165092720+jab0n@users.noreply.github.com>
2025-02-27 14:37:31 -0600

    Update Ejercicios_Haskell.hs

--- Ejercicios_Haskell.hs
+++ Ejercicios_Haskell.hs
@@ -1,5 +1,5 @@
 import Data.Map (fromList)
-import Statistics.Sample (mean, stdDev)
+-- import Statistics.Sample (mean, stdDev)
 
 -- 1)
 
@@ -38,7 +38,7 @@ contarPalabras frase = map (\palabra -> (palabra, length palabra)) (words frase)
 -- 4) Función que convierte asignaturas y calificaciones a un diccionario con calificaciones en palabras
 
 calificacionesEnTexto :: [(String, Float)] -> [(String, String)]
-calificacionesEnTexto asignaturas = map (\(asignatura, nota) -> (map toUpper asignatura, clasificarNota nota)) asignaturas
+calificacionesEnTexto asignaturas = map (\(asignatura, nota) -> (asignatura, clasificarNota nota)) asignaturas
   where
     clasificarNota nota
       | nota >= 95 = "Excelente"
@@ -54,36 +54,47 @@ moduloVector vector = sqrt (sum [x^2 | x <- vector])
 
 -- 6) Función que detecta los valores atípicos en una muestra
 
-valoresAtipicos :: [Float] -> [Float]
-valoresAtipicos muestra = filter (\x -> abs ((x - m) / desv) > 3) muestra
-  where
-    m = mean muestra
-    desv = stdDev muestra
+--valoresAtipicos :: [Float] -> [Float]
+--valoresAtipicos muestra = filter (\x -> abs ((x - m) / desv) > 3) muestra
+  --where
+    --m = mean muestra
+    --desv = stdDev muestra
 
 -- Prueba:
 
 main :: IO ()
 main = do
   -- 1)
+  print("Ejercicio 1")
   print descuento  -- Resultado con descuento
   print iva        -- Resultado con IVA
+  print("")
 
   -- 2)
+  print("Ejercicio 2")
   let lista = [1, 2, 3, 4, 5]
   print $ aplicarFuncionALista (*2) lista  -- Resultado: [2, 4, 6, 8, 10]
+  print("")
 
   -- 3)
+  print("Ejercicio 3")
   let frase = "Hola mundo en Haskell"
   print $ contarPalabras frase  -- Resultado: [("Hola", 4), ("mundo", 5), ("en", 2), ("Haskell", 7)]
+  print("")
 
   -- 4)
+  print("Ejercicio 4")
   let asignaturas = [("matematica", 96), ("historia", 80), ("quimica", 65)]
   print $ calificacionesEnTexto asignaturas  -- Resultado: [("MATEMATICA","Excelente"), ("HISTORIA","Bueno"), ("QUIMICA","Desempeño insuficiente")]
+  print("")
 
   -- 5)
+  print("Ejercicio 5")
   let vector = [3, 4]
   print $ moduloVector vector  -- Resultado: 5.0 (porque √(3² + 4²) = 5)
+  print("")
 
   -- 6)
-  let muestra = [10, 12, 13, 15, 100, 7, 9, 11]
-  print $ valoresAtipicos muestra  -- Resultado: [100]
+  print("Ejercicio 6")
+  --let muestra = [10, 12, 13, 15, 100, 7, 9, 11]
+  --print $ valoresAtipicos muestra  -- Resultado: [100]
