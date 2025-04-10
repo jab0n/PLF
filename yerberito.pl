bbbd29207eaa1cca11d68b29fec4ea19c272b424
jab0n <165092720+jab0n@users.noreply.github.com>
2025-04-10 08:18:34 -0600

    Add files via upload

---
+++ yerberito.pl
@@ -0,0 +1,353 @@
+hierba_botiquin(anis).
+hierba_botiquin(estrella).
+hierba_botiquin(menta).
+hierba_botiquin(arnica).
+hierba_botiquin(savila).
+hierba_botiquin(tila).
+hierba_botiquin(eucalipto).
+hierba_botiquin(yerbabuena).
+hierba_botiquin(manzanilla).
+hierba_botiquin(cola_de_caballo).
+hierba_botiquin(romero).
+hierba_botiquin(toronjil).
+hierba_botiquin(sanguinaria).
+hierba_botiquin(linaza).
+hierba_botiquin(hamamelis).
+hierba_botiquin(zarzaparrilla).
+hierba_botiquin(boloo).
+hierba_botiquin(diente_de_leon).
+hierba_botiquin(azahar).
+hierba_botiquin(malva).
+hierba_botiquin(marrubio).
+hierba_botiquin(rosal).
+
+hierba_medicinal(abrojo, [afecciones_del_pecho, higado, circulacion, inflamacion_del_ojo]).
+hierba_medicinal(acacia, [dolor_de_garganta, tos, bronquitis]).
+hierba_medicinal(acanto, [antivenenoso, emoliente]).
+hierba_medicinal(aceitilla, [cansancio_intelectual, depresion_nerviosa]).
+hierba_medicinal(achicoria, [diuretico, depurativo]).
+hierba_medicinal(sanguinaria, [depurativa,diuretica]).
+hierba_medicinal(sensitiva, [insomnio, cansancio]).
+hierba_medicinal(simonillo, [icteria,catarro_vias_biliares,colico_hepatico]).
+hierba_medicinal(tamarindo, [laxante,diuretico,refrescante]).
+hierba_medicinal(amapola_amarilla,[hipnotica,sedante,antidiarreica]).
+hierba_medicinal(anis, [indigestion,gases,nauseas]).
+hierba_medicinal(amate, [depuratico,estimulante]).
+hierba_medicinal(ruda, [abortiva, alivia_jaquecas, regula_menstruacion]).
+hierba_medicinal(ruibaro, [contra_disenteria, alivia_flatulencias, diuretico, estimulante_digestivo]).
+hierba_medicinal(salvia, [limpia_dientes, purifica_aliento]).
+hierba_medicinal(sen, [alivia_estrenimiento, antianemico]).
+hierba_medicinal(marihuana, [cancer, glaucoma, males_ojos, artritis, reumatismo]).
+hierba_medicinal(matarique, [diabetes, reumatismo, rinones_adoloridos]).
+hierba_medicinal(digitaria, [miocarditis, astenia, epilepsia]).
+hierba_medicinal(cardo_santo, [nubes_ojos]).
+hierba_medicinal(brionia, [lombrices]).
+hierba_medicinal(canela, [anemia]).
+hierba_medicinal(cedron, [tos]).
+hierba_medicinal(doradilla, [nefritis]).
+hierba_medicinal(epazote, [lombrices]).
+hierba_medicinal(enebro, [leucorrea]).
+hierba_medicinal(mangle, [diarrea, inflamaciones, heridas]).
+hierba_medicinal(manzanilla, [insomnio, agruras, higado_colitis, jaqueca, menstruacion, neuralgias, irritacion_ojo]).
+hierba_medicinal(marrubio, [asma, caida_cabello, obesidad, tos, reumatismo, vomitos]).
+hierba_medicinal(mastuerzo, [ciatica, tuberculosis]).
+hierba_medicinal(menta, [insomnio, lactancia, nauseas, neuralgias, vomitos, sarna]).
+
+propiedades_planta(sanguinaria, [depurativa, diuretica]).
+propiedades_planta(sensitiva, [somnifera, relajante]).
+propiedades_planta(simonillo, [ictericia, catarro_vias_biliares, colico_hepatico]).
+propiedades_planta(tamarindo, [laxante, diuretico, refrescante]).
+propiedades_planta(test, [laxante, diuretico, refrescante]).
+propiedades_planta(amapola_amarilla, [hipnotica,sedante,antidiarreica]).
+propiedades_planta(amate, [depuratico,estimulante]).
+propiedades_planta(anis, [desinfectante,autiflatulento]).
+propiedades_planta(ruda, [abortiva, alivia_jaquecas, regula_menstruacion]).
+propiedades_planta(ruibaro, [tapa, desinflamante, diuretico, estimulante]).
+propiedades_planta(salvia, [limpia_dientes, purifica_aliento]).
+propiedades_planta(sen, [alivia_estrenimiento, antianemico]).
+propiedades_planta(marihuana, [analgesica, antiinflamatoria]).
+propiedades_planta(matarique, [desinflamatoria, hipoglucemiante]).
+propiedades_planta(digitaria, [cardiotonica]).
+propiedades_planta(cardo_santo, [oftalmica]).
+propiedades_planta(brionia, [purgante, antiparasitaria]).
+propiedades_planta(canela, [estimulante, antimicrobiana]).
+propiedades_planta(cedron, [expectorante, calmante]).
+propiedades_planta(doradilla, [diuretica, desinflamatoria]).
+propiedades_planta(epazote, [antiparasitaria, carminativa]).
+propiedades_planta(enebro, [antiseptica, diuretica]).
+propiedades_planta(mangle, [astringente, antiinflamatoria]).
+propiedades_planta(manzanilla, [calmante, antiinflamatoria, digestiva]).
+propiedades_planta(marrubio, [expectorante, digestiva]).
+propiedades_planta(mastuerzo, [antiinflamatoria, expectorante]).
+propiedades_planta(menta, [digestiva, calmante, refrescante]).
+
+elementos_planta(tamarindo, [vitamina_A, vitamina_B, vitamina_C, acido_Acetico]).
+elementos_planta(abrohjo, [taninos, saponinas, flavonoides]).
+elementos_planta(acacia, [goma, mucilago, taninos]).
+elementos_planta(amapola_amarilla, [grasas, calorias, carbohidratos, fibras]).
+elementos_planta(amate, [antihelmintico, enzima_proteolitica_ficina]).
+elementos_planta(anis,[trans_anetol, anetol, albuminas]).
+elementos_planta(ruda, [vitamina_C]).
+elementos_planta(ruibaro, [aloe_emodina, diantronicos, senosidos]).
+elementos_planta(salvia, [monoterpenos, msesquiterpenos]).
+elementos_planta(sen, [estipulas]).
+
+preparacion(cocimiento, 'hervir 40 minutos, reposar 5 minutos').
+preparacion(infusion, 'pasar agua caliente por las hierbas').
+preparacion(maceracion, 'moler y agregar agua hasta que saque sustancias').
+preparacion(jarabe, 'hervir planta en agua por 10 minutos, agregar azucar y caramelizar 10 minutos, reposar').
+preparacion(tintura, 'se usa alcohol para sacar los nutrientes').
+preparacion(jugo, 'exprimir plantas con un trapo').
+preparacion(horchata, 'moler y agregar agua de poco a poco hasta mazilla').
+preparacion(desconocido, 'no se encuentra en el yerberito').
+preparacion(te, 'Te por 2 o 3 veces por dia').
+preparacion(mermelada, 'muela, hierba, agrege agua, hasta conseguir una pasta agregue asucar y listo!').
+preparacion(masticado, 'Se mastica para limpiar el aliento').
+preparacion(machacar, 'Se machaca la raiz para preparar un te').
+
+planta_preparacion(abrojo, cocimiento).
+planta_preparacion(acacia, desconocido).
+planta_preparacion(acanto, horchata).
+planta_preparacion(aceitilla, jugo).
+planta_preparacion(achicoria, cocimiento).
+planta_preparacion(sanguinaria, cocimiento).
+planta_preparacion(sensitiva, cocimiento).
+planta_preparacion(simonillo, cocimiento).
+planta_preparacion(tamarindo, jugo).
+planta_preparacion(amapola_amarilla, cocimiento).
+planta_preparacion(amate, desconocido).
+planta_preparacion(anis, cocimiento).
+planta_preparacion(ruda, te).
+planta_preparacion(ruibaro, mermelada).
+planta_preparacion(salvia, masticado).
+planta_preparacion(sen, machacar).
+planta_preparacion(marihuana, maceracion).
+planta_preparacion(matarique, cocimiento).
+planta_preparacion(digitaria, infusion).
+planta_preparacion(cardo_santo, cocimiento).
+planta_preparacion(brionia, cocimiento).
+planta_preparacion(canela, infusion).
+planta_preparacion(cedron, infusion).
+planta_preparacion(doradilla, cocimiento).
+planta_preparacion(epazote, cocimiento).
+planta_preparacion(enebro, infusion).
+planta_preparacion(mangle, cocimiento).
+planta_preparacion(manzanilla, infusion).
+planta_preparacion(marrubio, infusion).
+planta_preparacion(mastuerzo, cocimiento).
+planta_preparacion(menta, infusion).
+
+como_ingerir(abrojo, 'usar la raiz en agua como agua uso').
+como_ingerir(acacia, 'tomar te de sus hojas').
+como_ingerir(acanto, 'hojas de acanto aplicadas en piel').
+como_ingerir(aceitilla, 'tonico').
+como_ingerir(achicoria, 'tomar te de sus hojas secas').
+como_ingerir(sanguinaria, 'tomar en te 15 dias seguidos').
+como_ingerir(sensitiva, 'cocer las hojas raiz y tomar en te').
+como_ingerir(simonillo, 'usar toda la planta en te').
+como_ingerir(tamarindo, 'por medio de agua de su pulpa').
+como_ingerir(amapola_amarilla,'tomar el te de sus hojas solo durante la afeccion').
+como_ingerir(amate,'usar como emplastos en la sona de reumatismo').
+como_ingerir(anis,'tomarse como te').
+como_ingerir(ruda, 'Tecito 2 o 3 veces por dia').
+como_ingerir(ruibaro, 'Ingerir con pan bimbo untado con su mermelada').
+como_ingerir(salvia, 'Se mastica para limpiar el aliento').
+como_ingerir(sen, 'Se machaca la raiz para preparar un te').
+como_ingerir(marihuana, 'frotaciones externas con un poco de alcanfor o tomar como agua de uso').
+como_ingerir(matarique, 'tomar en te o como tintura').
+como_ingerir(digitaria, 'tomar en te bajo supervision medica').
+como_ingerir(cardo_santo, 'aplicar dos gotas en cada ojo').
+como_ingerir(brionia, 'tomar en te con precaucion por su toxicidad').
+como_ingerir(canela, 'tomar en te o agregar a alimentos').
+como_ingerir(cedron, 'tomar en te').
+como_ingerir(doradilla, 'tomar en te').
+como_ingerir(epazote, 'tomar en te').
+como_ingerir(enebro, 'tomar en te o usar en lavados').
+como_ingerir(mangle, 'tomar en te o aplicar en heridas').
+como_ingerir(manzanilla, 'tomar en te').
+como_ingerir(marrubio, 'tomar en te').
+como_ingerir(mastuerzo, 'tomar en te o aplicar en compresas').
+como_ingerir(menta, 'tomar en te').
+
+origen_planta(abrojo, 'Europa').
+origen_planta(acacia, 'África').
+origen_planta(acanto, 'Europa').
+origen_planta(aceitilla, 'América').
+origen_planta(achicoria, 'Europa').
+origen_planta(tamarindo, 'India').
+origen_planta(amapola_amarilla, 'Europa').
+origen_planta(amate, 'America').
+origen_planta(anis, 'Asia').
+origen_planta(ruda, 'Europa').
+origen_planta(ruibaro, 'Asia').
+origen_planta(salvia, 'America').
+origen_planta(sen, 'Africa').
+origen_planta(marihuana, 'India').
+origen_planta(matarique, 'Norte de Mexico').
+origen_planta(digitaria, 'Europa').
+origen_planta(cardo_santo, 'Mexico').
+origen_planta(brionia, 'Europa').
+origen_planta(canela, 'Asia').
+origen_planta(cedron, 'America').
+origen_planta(doradilla, 'America').
+origen_planta(epazote, 'America').
+origen_planta(enebro, 'Europa').
+origen_planta(mangle, 'America').
+origen_planta(manzanilla, 'Europa').
+origen_planta(marrubio, 'Europa').
+origen_planta(mastuerzo, 'Asia').
+origen_planta(menta, 'Europa').
+
+remedio(abceso, [malva]).
+remedio(abceso_hepatico, [zarzaparrilla]).
+remedio(acidez_estomacal, [anis, perejil]).
+remedio(acido_urico, [sanguinaria, limon, sauco]).
+remedio(acne, [arnica]).
+remedio(aftas, [llanten, fenogreco, zarzamora]).
+remedio(agotamiento, [salvia, tilo, valeriana]).
+remedio(agruras, [yerbabuena, manzanilla, jugo_de_limon, toronja]).
+remedio(albuminaria, [pinguica, quina_roja, encino_rojo]).
+remedio(alcoholismo, [pimiento]).
+remedio(almorranas, [salvia, sauco, cola_de_caballo, sanguinaria]).
+remedio(anemia, [ajenjo, quina, canela, germen_de_trigo]).
+remedio(anginas, [eucalipto, cebada, salvia, borraja]).
+remedio(anorexia, [ajenjo, genciana, yerbabuena]).
+remedio(arterosclerosis, [limon, genciana, cardo_santo, zarzaparrilla]).
+remedio(artritis, [arnica, chicalote, toronja, alcanfor, marihuana]).
+remedio(asma, [eucalipto, marrubio, oregano, salvia]).
+remedio(atonia_estomacal, [lupulo, eucalipto, cuasia]).
+remedio(bazo, [uva, cerezo]).
+remedio(boca, [malva, rosal, limon, salvia]).
+remedio(bronquitis, [eucalipto, borraja, gordolobo, tilo, rabano]).
+remedio(caida_cabello, [ortiga, espinosilla, marrubio, romero]).
+remedio(bronconeumonia, [gordolobo, eucalipto, ipecacuana, mostaza]).
+remedio(caries, [hiedra, cola_de_caballo]).
+remedio(caspa, [ortiga, limon, romero]).
+remedio(cancer_utero, [cuachalalate, llanten, siempreviva]).
+remedio(cancer, [marihuana]).
+remedio(glaucoma, [marihuana]).
+remedio(males_ojos, [marihuana]).
+remedio(ciatica, [mastuerzo, higuera, sauco]).
+remedio(circulacion, [toronjil, sanguinaria, salvia, hamamelis]).
+remedio(higado_colitis, [manzanilla]).
+remedio(higado_bilis, [lechuga, tila]).
+remedio(higado_ictericia, [papaloquelite, achicoria, berros, llanten]).
+remedio(hipertension_alta, [ajo, esparrago, alpiste, muerdago]).
+remedio(hipertension_baja, [miel, nuez_de_kola, crategus, acedera]).
+remedio(hipo, [anis, hinojo, tila, valeriana]).
+remedio(insomnio, [pasiflora, azahar, menta, manzanilla, lechuga, tila]).
+remedio(intestino, [gencina, melisa]).
+remedio(impotencia_sexual,[yohimbo,daniana, nuez_vomica, aguacate]).
+remedio(jaqueca,[manzanilla,aconito,valeriana,tila,chicalote]).
+remedio(lactancia,[hinojo,anis,menta,perejil,zanahoria]).
+remedio(laringitis,[aconito,borraja,cebolla,rosa,benjui,encino]).
+remedio(leucorrea,[encina,zarzaparrilla,pino,enebro,genciana]).
+remedio(lombrices,[ajenjo,ajo,cebolla,brionia,aguacate,papaya,epazote]).
+remedio(lumbago,[avena,cebada,tomillo,vernena]).
+remedio(llagas,[fenogreco,eucalipto,llanten,sanguinaria]).
+remedio(malaria,[quina,girasol,eucalipto,cardo_santo]).
+remedio(menopausia,[azahar,hamamelis,tila,quina_roja]).
+remedio(menstruacion,[azafran,hamamelis,belladana,anis_estrella,ruda,ajenjo,manzanilla,apio,hisopo,quina_amarilla,sabina,artemisa]).
+remedio(metorragia,[hamamelis,zoapatle,perejil,cuerecillo_centena]).
+remedio(muelas,[clavo,hiedra]).
+remedio(hemorragia_nazal,[ortiga,cola_de_caballo,ruda,eucalipto]).
+remedio(nauseas,[anis,ajenjo,menta,salvia]).
+remedio(neuralgias,[manzanilla,menta,valeriana,boldo]).
+remedio(neurastenia,[pasiflora,te_negro,mate,valeriana]).
+remedio(nefritis,[linaza,grama_cebada,llanten,doradilla,esparrago,ruda]).
+remedio(obesidad,[toronjil, marrudoro, malca, limon, esparrago]).
+remedio(oidos,[boldo, aceite_de_oliva, llante, hiedra]).
+remedio(conjuntivitis,[manzanilla, limon, llante, salvia, ruda, rosal]).
+remedio(irritacion_ojo,[manzanilla, limon, llante, salvia, ruda, rosal, cardo_santo]).
+remedio(nubes_ojos, [cardo_santo]).
+remedio(paludismo,[ajenjo, quina]).
+remedio(pecas,[berro, benciana, rabano, papaya]).
+remedio(pies_olorosos,[laurel, encina]).
+remedio(piquetes_de_abeja,[miel,perejil,cebolla,puerro]).
+remedio(piquetes_de_arania,[fresno,ipecacuana]).
+remedio(piquetes_de_mosco,[alcanfor, perejil, hamamelis]).
+remedio(piquetes_de_vibora,[anagalida]).
+remedio(pleuresia,[jengibre, linaza, cardo_santo, girasol]).
+remedio(piorrea,[ipecacuana]).
+remedio(prostata,[cola_de_caballo]).
+remedio(pulmonia,[eucalipto, ocote, gordolobo, borraja, sauco]).
+remedio(quemaduras,[linaza, cebolla, hiedra, gordolobo]).
+remedio(raquitismo,[nogal]).
+remedio(reumatismo,[ajo, apio, borrajal, gordolobo, pino, romero, fanguinaria, marrubio, tabaco, marihuana, matarique]).
+remedio(riniones,[cabellos_de_elote, cola_de_caballo, apio, matarique]).
+remedio(diabetes, [matarique]).
+remedio(miocarditis, [digitaria]).
+remedio(astenia, [digitaria]).
+remedio(epilepsia, [digitaria]).
+remedio(ronquera,[eucalipto, pino, gordolobo]).
+remedio(sabaniones,[ajo, cebolla]).
+remedio(mal_de_san_vito,[estafiare, valeriana]).
+remedio(sarampion,[borraja, ortiga, sauco]).
+remedio(sarna,[ajo,alcanfor,menta,tomillo,romero]).
+remedio(sarpullido, [encina, salvia, tila]).
+remedio(sed, [limon, tamarindo, pirul]).
+remedio(solitaria, [semilla_de_calabaza, granado, coquito_de_aceite, helecho_macho]).
+remedio(sudoracion_exesiva, [encina]).
+remedio(tifoaidea,[alcanfor, borraja, quina, canela, romero, salvia]).
+remedio(tinia,[berro, tila, tamarindo, salvia]).
+remedio(tos, [eucalipto, capulin, cedron, salvia, malva, marrubio]).
+remedio(tos_ferina,[gelsemio, quina, rabano, videta]).
+remedio(tuberculosis,[mastuerzo, berro, ajo, eucalipto, pirul, pino, roble, platano_morado]).
+remedio(ulcera,[cuachalalate, sanginaria, cola_de_caballo, girasol]).
+remedio(urticaria,[limon, ruibarbo]).
+remedio(varices,[hamamelis, castanio_de_indias, llanten, taranjil]).
+remedio(vejiga,[apio, cipres, cola_de_caballo, ortiga, malva]).
+remedio(verrugas,[leche_de_higuera, cebolla, nogal]).
+remedio(vertigos,[albhaca, espino]).
+remedio(vomitos,[menta, tila, marrubio, valeriana, salvia]).
+remedio(voz,[cilantro, ajo, limon, pino]).
+remedio(carencia_de_vitaminas,[alfalfa, espinacas, acelga, berro, cebolla, limon, zanahoria, aceite_de_bacalao]).
+
+medicamento_Planta(sanguinaria,'enjuague_bucal').
+medicamento_Planta(tamarindo,'laxantes').
+medicamento_Planta(amapola_amarilla,'suplementos_del_suenio').
+medicamento_Planta(anis,'anticolicos').
+medicamento_Planta(ruda,'fitoterapeuticos').
+medicamento_Planta(salvia,'pastillas_garganta').
+medicamento_Planta(sen,'laxantes').
+
+obtener_remedios(Remedios) :- findall(Remedio, remedio(Remedio, _), Remedios).
+
+metodo_preparacion(Planta, Metodo, Descripcion) :- planta_preparacion(Planta, Metodo), preparacion(Metodo, Descripcion).
+
+mostrar_informacion_planta(Planta) :-
+    (   hierba_medicinal(Planta, Informacion)
+    ->  atomics_to_string(Informacion, ', ', InfoString),
+        metodo_preparacion(Planta, Metodo, MetodoDescripcion),
+        (   como_ingerir(Planta, ComoIngerir) -> true ; ComoIngerir = 'No disponible'),
+        (   propiedades_planta(Planta, Propiedades) -> atomics_to_string(Propiedades, ', ', PropiedadesString) ; PropiedadesString = 'No disponibles'),
+        (   elementos_planta(Planta, Elementos) -> atomics_to_string(Elementos, ', ', ElementosString) ; ElementosString = 'No disponibles'),
+        (   origen_planta(Planta, Origen) -> true ; Origen = 'No disponible'),
+        format('Nombre de la planta: ~w~n', [Planta]),
+        format('Afecciones que trata: ~w~n', [InfoString]),
+        format('Metodo de preparacion: ~w~n', [Metodo]),
+        format('Preparacion: ~w~n', [MetodoDescripcion]),
+        format('Como ingerir: ~w~n', [ComoIngerir]),
+        format('Propiedades: ~w~n', [PropiedadesString]),
+        format('Elementos: ~w~n', [ElementosString]),
+        format('Origen: ~w~n', [Origen])
+    ;   write('Planta no encontrada'), nl
+    ).
+
+mostrar_informacion_remedio(Remedio) :-
+    (   remedio(Remedio, Plantas)
+    ->  atomics_to_string(Plantas, ', ', PlantasString),
+        format('Enfermedad: ~w~n', [Remedio]),
+        format('Plantas que lo tratan: ~w~n', [PlantasString])
+    ;   write('Enfermedad no encontrada'), nl
+    ).
+
+mostrar_plantas_botiquin :-
+    findall(Planta, hierba_botiquin(Planta), Plantas),
+    atomics_to_string(Plantas, ', ', PlantasString),
+    format('Plantas del mini botiquin: ~w~n', [PlantasString]).
+
+mostrar_lista_enfermedades :-
+    findall(Enfermedad, remedio(Enfermedad, _), Enfermedades),
+    atomics_to_string(Enfermedades, ', ', EnfermedadesString),
+    format('Lista de padecimientos: ~w~n', [EnfermedadesString]).
